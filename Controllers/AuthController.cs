ffc3298837612551e967fff1a30dc8889a0bebf1
jollyjerr <jollyjerr@gmail.com>
2019-10-10 14:28:28 -0600

    adds auth controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Asp.NetCore.Mvc;
+
+namespace footprints.Controllers
+{
+    public class AuthController : Controller
+    {
+
+    }
+}
