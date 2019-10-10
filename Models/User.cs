319fec98e12cbc7611997d77e7040fcdee8dbaf6
jollyjerr <jollyjerr@gmail.com>
2019-10-10 13:26:42 -0600

    adds user model

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace footprints.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
