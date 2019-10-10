cbdb532fa32719f7140146422b8a4422eb3a0457
jollyjerr <jollyjerr@gmail.com>
2019-10-10 15:06:42 -0600

    correct edition - adds user creation ability

---
+++ Dtos/UserForLoginDto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace footprints.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+} 
