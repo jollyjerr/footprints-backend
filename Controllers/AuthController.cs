3f15a7383c1ddac3cef16979269f392e064d90e3
jollyjerr <jollyjerr@gmail.com>
2019-10-10 15:00:49 -0600

    can add users

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -2,12 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Asp.NetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using footprints.Models;
+using footprints.Data;
+using footprints.Dtos;
 
 namespace footprints.Controllers
 {
+    [Route("api/[controller]")]
     public class AuthController : Controller
     {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
 
+        [HttpPost("register")] //<host>/api/auth/register
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto){ //Data Transfer Object containing username and password.
+            // validate request
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower(); //Convert username to lower case before storing in database.
+
+            if(await _repo.UserExists(userForRegisterDto.Username)) 
+                return BadRequest("Username is already taken");
+
+            var userToCreate = new User{
+                Username = userForRegisterDto.Username
+            };
+
+            var createUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
     }
 }
---
+++ Dtos/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace footprints.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        
+        [Required]
+        [StringLength(30, MinimumLength =4, ErrorMessage = "You must specify a password between 4 and 30 characters.")]
+        public string Password { get; set; }
+    }
+}
