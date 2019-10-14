a5f519cebb642b42c2ab55138df1ad11f89d9c92
jollyjerr <jollyjerr@gmail.com>
2019-10-14 14:16:09 -0600

    stale

--- Startup.cs
+++ Startup.cs
@@ -73,7 +73,7 @@ namespace footprints
                 app.UseDeveloperExceptionPage();
             }
 
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
