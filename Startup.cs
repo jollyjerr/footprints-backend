633dc0fad8e6f8df5f75761c310073dbfb03e3d6
jollyjerr <jollyjerr@gmail.com>
2019-10-16 15:56:40 -0600

    functional deployment

--- Startup.cs
+++ Startup.cs
@@ -43,6 +43,8 @@ namespace footprints
                     ValidateAudience = false
                 };
             });
+
+            services.BuildServiceProvider().GetService<DataContext>().Database.Migrate();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
