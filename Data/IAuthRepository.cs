e59cb8462416ab6010a1b7d4a726877b3cf78468
jollyjerr <jollyjerr@gmail.com>
2019-10-21 10:23:48 -0600

    WIP: failed reg

--- Data/IAuthRepository.cs
+++ Data/IAuthRepository.cs
@@ -6,7 +6,6 @@ namespace footprints.Data
 {
     public interface IAuthRepository
     {
-        Task<User> Register(User user, string password);
         Task<User> Login(string username, string password);
         Task<bool> UserExists(string username);
         Task<User> Register(User userToCreate, string password, Vehicle vehicleToCreate, House houseToCreate);
