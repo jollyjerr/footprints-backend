56fa4ed8872200e1f5b3e148d97cfb229b13ff5c
jollyjerr <jollyjerr@gmail.com>
2019-10-20 21:35:49 -0600

    proper db associations

---
+++ Migrations/20191021033056_ChangeStorageType.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace footprints.Migrations
+{
+    public partial class ChangeStorageType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
