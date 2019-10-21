42797a17047336b2a8b4baafb83fb1bce20ae0d2
jollyjerr <jollyjerr@gmail.com>
2019-10-21 11:27:16 -0600

    establish vehicle class and inject repo

---
+++ Controllers/VehicleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using footprints.Models;
+using footprints.Data;
+using footprints.Dtos;
+
+namespace footprints.Controllers
+{
+    [Route("api/[controller]")]
+    public class VehicleController : Controller
+    {
+        private readonly IVehicleRepository _repo;
+        private readonly IConfiguration _config;
+        public VehicleController(IVehicleRepository repo, IConfiguration config)
+        {
+            _repo = repo;
+            _config = config;
+        }
+    }
+}
---
+++ Data/IVehicleRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using footprints.Models;
+
+namespace footprints.Data
+{
+    public interface IVehicleRepository
+    {
+        Task<Vehicle> Register(Vehicle vehicle);
+    }
+}
